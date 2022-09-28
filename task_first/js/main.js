f9866f60e75d9fd630f4596b4bed325652246bc0
priyankavithalpara309 <113996298+priyankavithalpara309@users.noreply.github.com>
2022-09-28 15:36:03 +0530

    Add files via upload

---
+++ task_first/js/main.js
@@ -0,0 +1,8 @@
+function scrolll(){
+    var left=document.querySelector(".scroll-images");
+    left.scrollBy(-150,0);    
+}
+function scrollr(){
+    var right=document.querySelector(".scroll-images");
+    right.scrollBy(150,0);
+}
